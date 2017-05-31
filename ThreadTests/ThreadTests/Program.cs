9a0dd60fd4ff4ad5423f61d663ba72454e8b4737
VitoDegt <geret91@mail.ru>
2017-05-31 20:00:55 +0300

    Add files via upload

---
+++ ThreadTests/ThreadTests/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ThreadTests
+{
+    
+
+    class FileWatcher
+    {
+
+
+        private string _path;
+        private string source;
+        private bool isOperation = true;
+
+        public FileWatcher(string path)
+        {
+            path = _path;
+        }
+        public void CheckFile(string path)
+        {
+            if (isOperation)
+            {
+                File.GetLastWriteTime(_path); 
+            }
+        }
+
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
